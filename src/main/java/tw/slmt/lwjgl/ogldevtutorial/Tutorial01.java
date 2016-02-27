1a69c13c27e0bc9e6065e9860f8a047bd3350822
slmt <sam123456777@gmail.com>
2016-02-27 15:47:38 +0800

    Clean up some unneccesary stuffs

--- src/main/java/tw/slmt/lwjgl/ogldevtutorial/Tutorial01.java
+++ src/main/java/tw/slmt/lwjgl/ogldevtutorial/Tutorial01.java
@@ -4,80 +4,74 @@ import java.nio.IntBuffer;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryUtil;
 
 public class Tutorial01 {
-	
+
 	// Note that this program should run with VM argument
 	// '-XstartOnFirstThread' for OS X
-	
-	public static void main(String[] args)
-	{
+
+	private static final String WINDOW_TITLE = "Tutorial 1 - Open A Window";
+
+	public static void main(String[] args) {
 		GLFW.glfwSetErrorCallback(GLFWErrorCallback.createPrint());
 		if (GLFW.glfwInit() != GLFW.GLFW_TRUE)
 			System.exit(-1);
-		
-		long windowHandle = GLFW.glfwCreateWindow(1024, 768, 
-				"Tutorial 1 - Open a window", MemoryUtil.NULL, MemoryUtil.NULL);
+
+		long windowHandle = GLFW.glfwCreateWindow(1024, 768, WINDOW_TITLE,
+				MemoryUtil.NULL, MemoryUtil.NULL);
 		if (windowHandle == MemoryUtil.NULL) {
 			GLFW.glfwTerminate();
 			System.exit(-1);
 		}
-		
+
 		GLFW.glfwMakeContextCurrent(windowHandle);
 		GLFW.glfwSwapInterval(1);
-		GLFW.glfwSetKeyCallback(windowHandle, new GLFWKeyCallback() {
 
-			@Override
-			public void invoke(long window, int key, int scancode, int action,
-					int mods) {
-				System.out.println("Press key " + ((char) key));
-			}
-			
-		});
-		
 		// This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		//
+		// - https://www.lwjgl.org/guide
 		GL.createCapabilities();
-		
-		while (GLFW.glfwWindowShouldClose(windowHandle) != GLFW.GLFW_TRUE)
-	    {
+
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+		while (GLFW.glfwWindowShouldClose(windowHandle) != GLFW.GLFW_TRUE) {
 			IntBuffer widthBuf = MemoryUtil.memAllocInt(1);
 			IntBuffer heightBuf = MemoryUtil.memAllocInt(1);
-			
-	        GLFW.glfwGetFramebufferSize(windowHandle, widthBuf, heightBuf);
-			
-	        draw(widthBuf.get(), heightBuf.get());
-	        
-	        GLFW.glfwSwapBuffers(windowHandle);
-	        GLFW.glfwPollEvents();
-	    }
-		
+
+			GLFW.glfwGetFramebufferSize(windowHandle, widthBuf, heightBuf);
+
+			renderScene(widthBuf.get(), heightBuf.get());
+
+			GLFW.glfwSwapBuffers(windowHandle);
+			GLFW.glfwPollEvents();
+		}
+
 		GLFW.glfwDestroyWindow(windowHandle);
 		GLFW.glfwTerminate();
 	}
-	
-	private static void draw(int width, int height) {
+
+	private static void renderScene(int width, int height) {
 		float ratio = (float) width / (float) height;
 		float time = (float) GLFW.glfwGetTime();
-		
+
 		GL11.glViewport(0, 0, width, height);
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-        
+
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        
+
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		GL11.glLoadIdentity();
 		GL11.glRotatef(time * 50.f, 0.f, 1.f, 0.f);
-        
+
 		GL11.glBegin(GL11.GL_TRIANGLES);
 		GL11.glColor3f(1.f, 0.f, 0.f);
 		GL11.glVertex3f(-0.6f, -0.4f, 0.f);
--- src/main/java/tw/slmt/lwjgl/ogldevtutorial/Tutorial02.java
+++ src/main/java/tw/slmt/lwjgl/ogldevtutorial/Tutorial02.java
@@ -6,7 +6,6 @@ import java.nio.IntBuffer;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
@@ -14,85 +13,81 @@ import org.lwjgl.opengl.GL20;
 import org.lwjgl.system.MemoryUtil;
 
 public class Tutorial02 {
-	
+
 	// Note that this program should run with VM argument
 	// '-XstartOnFirstThread' for OS X
-	
+
+	private static final String WINDOW_TITLE = "Tutorial 2 - Hello Dot!";
+
 	private static int vboId;
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		GLFW.glfwSetErrorCallback(GLFWErrorCallback.createPrint());
 		if (GLFW.glfwInit() != GLFW.GLFW_TRUE)
 			System.exit(-1);
-		
-		long windowHandle = GLFW.glfwCreateWindow(1024, 768, 
-				"Tutorial 2 - Hello Dot!", MemoryUtil.NULL, MemoryUtil.NULL);
+
+		long windowHandle = GLFW.glfwCreateWindow(1024, 768, WINDOW_TITLE,
+				MemoryUtil.NULL, MemoryUtil.NULL);
 		if (windowHandle == MemoryUtil.NULL) {
 			GLFW.glfwTerminate();
 			System.exit(-1);
 		}
-		
+
 		GLFW.glfwMakeContextCurrent(windowHandle);
 		GLFW.glfwSwapInterval(1);
-		GLFW.glfwSetKeyCallback(windowHandle, new GLFWKeyCallback() {
 
-			@Override
-			public void invoke(long window, int key, int scancode, int action,
-					int mods) {
-				System.out.println("Press key " + ((char) key));
-			}
-			
-		});
-		
 		// This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		//
+		// - https://www.lwjgl.org/guide
 		GL.createCapabilities();
-		
-		loadData();
-		
-		while (GLFW.glfwWindowShouldClose(windowHandle) != GLFW.GLFW_TRUE)
-	    {
+
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+		createVertexBuffer();
+
+		while (GLFW.glfwWindowShouldClose(windowHandle) != GLFW.GLFW_TRUE) {
 			IntBuffer widthBuf = MemoryUtil.memAllocInt(1);
 			IntBuffer heightBuf = MemoryUtil.memAllocInt(1);
-			
-	        GLFW.glfwGetFramebufferSize(windowHandle, widthBuf, heightBuf);
-			
-	        draw();
-	        
-	        GLFW.glfwSwapBuffers(windowHandle);
-	        GLFW.glfwPollEvents();
-	    }
-		
+
+			GLFW.glfwGetFramebufferSize(windowHandle, widthBuf, heightBuf);
+
+			renderScene();
+
+			GLFW.glfwSwapBuffers(windowHandle);
+			GLFW.glfwPollEvents();
+		}
+
 		GLFW.glfwDestroyWindow(windowHandle);
 		GLFW.glfwTerminate();
 	}
-	
-	private static void loadData() {
+
+	private static void createVertexBuffer() {
 		// Note that we should use BufferUtil instead of MemoryUtil here.
 		// Or an undefined behavior will happen.
-		float[] vertices = new float[]{0.0f, 0.0f, 0.0f};
+		float[] vertices = new float[] { 0.0f, 0.0f, 0.0f };
 		FloatBuffer vertBuffer = BufferUtils.createFloatBuffer(vertices.length);
 		vertBuffer.put(vertices);
 		vertBuffer.rewind();
-		
-		vboId = GL15.glGenBuffers(); // This function is specialized for the case of generating one buffer
+
+		vboId = GL15.glGenBuffers(); // This function is specialized for the
+										// case of generating one buffer
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertBuffer, GL15.GL_STATIC_DRAW);
 	}
-	
-	private static void draw() {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-        
-        GL20.glEnableVertexAttribArray(0);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
-        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-        
-        GL11.glDrawArrays(GL11.GL_POINTS, 0, 1);
-        
-        GL20.glDisableVertexAttribArray(0);
+
+	private static void renderScene() {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+		GL20.glEnableVertexAttribArray(0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+
+		GL11.glDrawArrays(GL11.GL_POINTS, 0, 1);
+
+		GL20.glDisableVertexAttribArray(0);
 	}
 }
